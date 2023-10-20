1c4a14fcefedd3221be1a38298a4c545f604aa4a
juagilei <juagilei@gmail.com>
2023-10-20 13:32:01 +0200

    ejercicio2 completado

---
+++ introduction-react-02/src/components/Ejercicio2/Ejercicio2.jsx
@@ -0,0 +1,16 @@
+// importo lo que necesito
+import TituloEjercicio from "../TituloEjercicio"
+import Formulario from "./Formulario"
+// Declaro una función ejercicio2 la cual contendrá Titulo ejercicio y formulario
+export default function Ejercicio2() {
+    
+return (
+    <section className="caja-ejercicio">
+      <TituloEjercicio>Este es el ejercicio 2</TituloEjercicio>
+      <Formulario />
+    </section>
+)
+}
+
+
+
